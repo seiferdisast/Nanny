8b20913ba5f572ead6739c525f103afddd689df1
Ingdarwin <104314776+Ingdarwin@users.noreply.github.com>
2022-10-30 23:57:20 -0500

    Create activities, register and recovery

---
+++ app/src/main/java/com/example/nanny/RecoveryKey.kt
@@ -0,0 +1,11 @@
+package com.example.nanny
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoveryKey : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery_key)
+    }
+}
