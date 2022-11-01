1474b9c8ab76a9865147744fc87ffb8079d33b28
seiferdisast <seifer.code@gmail.com>
2022-11-01 18:04:34 -0500

    fix bug manifest

--- app/src/main/java/com/example/nanny/MainActivity.kt
+++ app/src/main/java/com/example/nanny/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.nanny.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_Nanny)
+
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         val view=binding.root
