4da8b541395d512bbff057426917ef3b6eb75975
Ingdarwin <104314776+Ingdarwin@users.noreply.github.com>
2022-11-01 12:20:34 -0500

    Connect activities, login register and recovery

--- app/src/main/java/com/example/nanny/MainActivity.kt
+++ app/src/main/java/com/example/nanny/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.nanny
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.nanny.databinding.ActivityMainBinding
+import com.example.nanny.databinding.ActivitySplashaniBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Nanny)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+        binding.btnLoginMain.setOnClickListener{
+            startActivity(Intent(this,login::class.java))
+        }
+        binding.labelRegisterMain.setOnClickListener{
+            startActivity(Intent(this,Register::class.java))
+        }
+        binding.btnAboutUsMain.setOnClickListener{
+            startActivity(Intent(this,Nosotros::class.java))
+        }
     }
 }
