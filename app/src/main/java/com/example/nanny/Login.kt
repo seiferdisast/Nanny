4757e1d2dd52b63f78abd8993d0aef045df1235a
howguitar <howguitars@hotmail.com>
2022-11-13 21:23:39 -0500

    update login

--- app/src/main/java/com/example/nanny/Login.kt
+++ app/src/main/java/com/example/nanny/Login.kt
@@ -6,43 +6,66 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.room.Room
+import com.example.nanny.data.UserData
 import com.example.nanny.databinding.ActivityLoginBinding
+import com.example.nanny.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    private lateinit var database: UserData
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_login)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
+
+        //firebaseAuth=Firebase.auth
+        setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        database= Room.databaseBuilder(
+            application,UserData::class.java,UserData.DATABASE_NAME).allowMainThreadQueries().build()
+
+        val view=binding.root
         setContentView(view)
 
-        fun validate() {
-            val datauser: String = binding.inputEmailLogin.text.toString()
-            val datapass: String = binding.inputPasswordLogin.text.toString()
+        fun validate(){
+            println("hola mundo")
+            val datauser:String=binding.inputEmailLogin.text.toString()
+            val datapass:String=binding.inputPasswordLogin.text.toString()
 
-            val datos = getSharedPreferences("datauser", Context.MODE_PRIVATE)
-            val user = datos.getString("nombreusuario", "")
-            val password = datos.getString("claveusuario", "")
-            if (user!!.isEmpty()) {
+            val datos =getSharedPreferences("datauser",Context.MODE_PRIVATE)
+            val user=datos.getString("nombreusuario","")
+            val password=datos.getString("claveusuario","")
+            if (user!!.isEmpty()){
                 binding.inputEmailLogin.setHint("Ingrese el correo")
-            } else if (password!!.isEmpty()) {
+            }
+
+            else if(password!!.isEmpty()){
+                println("ingrese clave")
                 //binding.inputPasswordLogin.setHint("ingrese clave")
                 //binding.inputPasswordLogin.setHintTextColor(Color.RED)
-                Toast.makeText(this, "ingrese clave", Toast.LENGTH_SHORT).show()
-            } else if (user.equals(datauser) && password.equals(datapass)) {
-                val intent = Intent(this, User::class.java)
+                Toast.makeText(this,"ingrese clave",Toast.LENGTH_SHORT).show()
+            }
+            else if(user.equals(datauser) && password.equals(datapass)){
+                println("Datos Correctos")
+                val intent=Intent(this,User::class.java)
                 intent.putExtra("nombre", datauser)
                 startActivity(intent)
-                Toast.makeText(this, "Datos Correctos", Toast.LENGTH_SHORT).show()
-            } else {
+                Toast.makeText(this,"Datos Correctos",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                println("Datos incorrectos")
                 binding.inputEmailLogin.setText("")
-                Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Datos incorrectos",Toast.LENGTH_SHORT).show()
             }
 
         }
 
-        binding.btnEnterLogin.setOnClickListener {
+        binding.btnEnterLogin.setOnClickListener{
             /*val usuario:String=binding.inputEmailLogin.text.toString()
             val clave:String=binding.inputPasswordLogin.text.toString()
             if (usuario=="pepe" && clave=="123"){
@@ -51,11 +74,51 @@ class Login : AppCompatActivity() {
             else{
                 Toast.makeText(this,"Datos incorrectos",Toast.LENGTH_SHORT).show()
             }*/
-
-            validate()
+            validateWithDB()
+            //validate()
+            //authlogin(binding.inputEmailLogin.text.toString(),binding.inputPasswordLogin.text.toString())
         }
-        binding.labelRecoveryPassLogin.setOnClickListener {
-            startActivity(Intent(this, RecoveryKey::class.java))
+        binding.labelRecoveryPassLogin.setOnClickListener{
+            startActivity(Intent(this,RecoveryKey::class.java))
         }
     }
+
+    private fun authlogin(email:String,password:String){
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                task->
+            if (task.isSuccessful){
+                Toast.makeText(this,"datos ok",Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"datos incorrectos",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
+
+    fun validateWithDB(){
+        val user = binding.inputEmailLogin.text.toString()
+        val password=binding.inputPasswordLogin.text.toString()
+        val consultUser=database.userDao.consult(user)
+        println(consultUser)
+        if (consultUser!=null){
+            println(consultUser.pass)
+            if (consultUser.pass==password){
+                Toast.makeText(this,"datos ok",Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,User::class.java))
+            }
+            else{
+                Toast.makeText(this,"clave incorrecta",Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            Toast.makeText(this,"verifique sus datos",Toast.LENGTH_LONG).show()
+
+        }
+
+
+    }
+
 }
