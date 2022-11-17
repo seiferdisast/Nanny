e78e9af0f074932fa4dd41f999190973651ee715
Ingdarwin <104314776+Ingdarwin@users.noreply.github.com>
2022-11-16 20:58:04 -0500

    update Recovery FB

--- app/src/main/java/com/example/nanny/RecoveryKey.kt
+++ app/src/main/java/com/example/nanny/RecoveryKey.kt
@@ -6,30 +6,49 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.nanny.databinding.ActivityLoginBinding
 import com.example.nanny.databinding.ActivityRecoveryKeyBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoveryKey : AppCompatActivity() {
     private lateinit var binding: ActivityRecoveryKeyBinding
-
+    private lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_login)
         binding = ActivityRecoveryKeyBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        firebaseAuth=Firebase.auth
+
+
 
         binding.btnSendRecovery.setOnClickListener {
-            val email: String = binding.inputEmailRecovery.text.toString()
-            if (email == "pepe") {
-                startActivity(Intent(this, Login::class.java))
-                Toast.makeText(
-                    this,
-                    "Revisa la bandeja de entrada de tu correo asociado.",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                Toast.makeText(this, "Correo no registrado, intentelo de nuevo.", Toast.LENGTH_SHORT).show()
-                binding.inputEmailRecovery.text = null
+            resetPassword(binding.inputEmailRecovery.text.toString())
+        }
+    }
+    private fun resetPassword(email:String){
+        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(){
+            task->
+            if(task.isSuccessful){
+                Toast.makeText(this,"su correo de recuperacion",Toast.LENGTH_LONG).show()
             }
         }
     }
+
+    private fun resertExamplePass(){
+        val email: String = binding.inputEmailRecovery.text.toString()
+        if (email == "pepe") {
+            startActivity(Intent(this, Login::class.java))
+            Toast.makeText(
+                this,
+                "Revisa la bandeja de entrada de tu correo asociado.",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            Toast.makeText(this, "Correo no registrado, intentelo de nuevo.", Toast.LENGTH_SHORT).show()
+            binding.inputEmailRecovery.text = null
+        }
+    }
+
 }
