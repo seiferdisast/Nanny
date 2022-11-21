9d6ea25852546aa581acc9486f37126c14775e3f
seiferdisast <seifer.code@gmail.com>
2022-11-21 01:36:03 -0500

    implementation DB on name/phone of user view

--- app/src/main/java/com/example/nanny/User.kt
+++ app/src/main/java/com/example/nanny/User.kt
@@ -3,25 +3,23 @@ package com.example.nanny
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.nanny.databinding.ActivityLoginBinding
 import com.example.nanny.databinding.ActivityUserBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 class User : AppCompatActivity() {
     private lateinit var binding:ActivityUserBinding
     private val bd=FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val bundle=intent.extras
+        val data=bundle?.getString("ide")
+        consulUserData(data?:"")
+
         super.onCreate(savedInstanceState)
 
         binding= ActivityUserBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
-        obtenerdatos()
-        val bundle=intent.extras
-        val data=bundle?.getString("ide")
-        consulUserData(data?:"")
-
-
 
         binding.btnOurNannysUser.setOnClickListener{
             startActivity(Intent(this,OurNannys::class.java))
@@ -33,16 +31,10 @@ class User : AppCompatActivity() {
 
     }
 
-    fun obtenerdatos(){
-        val nombreus=binding.labelNameUser
-        val bundle=intent.extras
-        val dato=bundle?.getString("nombre")
-        nombreus.setText(dato)
-    }
-
     private fun consulUserData(id:String){
-        bd.collection("ojo aqui nombre colleccion en FB").document(id).get().addOnSuccessListener {
-            binding.labelNameUser.setText(it.get("nombre")as String?)
+        bd.collection("appUser").document(id).get().addOnSuccessListener {
+            binding.labelNameUser.setText(it.get("names")as String?)
+            binding.labelPhoneUser.setText(it.get("phone")as String?)
         }
 
     }
