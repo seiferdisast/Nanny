8c6dc69ff1ba9d9f140bbe8c32310e0ffa1f607c
seiferdisast <seifer.code@gmail.com>
2022-11-20 23:36:24 -0500

    integration id whit database FireStore

--- app/src/main/java/com/example/nanny/Register.kt
+++ app/src/main/java/com/example/nanny/Register.kt
@@ -1,7 +1,7 @@
 package com.example.nanny
 
-import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +14,6 @@ import androidx.core.content.FileProvider
 import androidx.room.Room
 import com.example.nanny.data.UserData
 import com.example.nanny.databinding.ActivityRegisterBinding
-import com.example.nanny.model.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -24,18 +23,29 @@ import java.io.File
 class Register : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private lateinit var database:UserData
+    private lateinit var database: UserData
     private lateinit var firebaseAuth: FirebaseAuth
-    private val db=FirebaseFirestore.getInstance()
+    private lateinit var file: File
+    private val db = FirebaseFirestore.getInstance()
+    private var id: String? = ""
+    private var bitmap: Bitmap? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
-        firebaseAuth=Firebase.auth
 
-        database= Room.databaseBuilder(
-            application,UserData::class.java,UserData.DATABASE_NAME).allowMainThreadQueries().build()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        firebaseAuth = Firebase.auth
+
+
+        database = Room.databaseBuilder(
+            application, UserData::class.java, UserData.DATABASE_NAME
+        ).allowMainThreadQueries().build()
+
 
         setContentView(binding.root)
+
+
         binding.labelTakeAPhotoRegister.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
                 it.resolveActivity(packageManager).also { component ->
@@ -43,172 +53,77 @@ class Register : AppCompatActivity() {
                     val photoUri: Uri = FileProvider.getUriForFile(
                         this, BuildConfig.APPLICATION_ID + ".fileprovider", file
                     )
-                    it.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+                    it.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
                 }
             }
             openCamera.launch(intent)
         }
 
+        binding.btnSaveRegister.setOnClickListener {
+            val emailValue = binding.inputEmailRegister.text.toString()
+            val passwordValue = binding.inputPasswordRegister.text.toString()
 
-        fun savedata(){
-            val user:String = binding.inputEmailRegister.text.toString()
-            val password:String=binding.inputPasswordRegister.text.toString()
-            val address:String=binding.inputAdressRegister.text.toString()
-            val name:String=binding.inputNamesRegister.text.toString()
-            val phone:String=binding.inputPhoneRegister.text.toString()
-            val rol:String=binding.inputRollRegister.text.toString()
+            if (emailValue.isNotEmpty() && passwordValue.isNotEmpty()) {
+                if (passwordValue.length >= 6) {
+                    userRegisterFB(emailValue, passwordValue)
 
-            val datos=getSharedPreferences("datauser",Context.MODE_PRIVATE)
-            val editor=datos.edit()
-            editor.putString("nombreusuario",user)
-            editor.putString("claveusuario",password)
-            editor.putString("addressUsuario",address)
-            editor.putString("nameusuario",name)
-            editor.putString("phoneusuario",phone)
-            editor.putString("rolusuario",rol)
-            editor.commit()
-
-            startActivity(Intent(this,MainActivity::class.java))
-        }
-
-        binding.btnSaveRegister.setOnClickListener{
-            //savedata()
-            //saveuserDB()
-            //Toast.makeText(this,"Datos guardados",Toast.LENGTH_LONG).show()
-            val usu=binding.inputEmailRegister.text.toString()
-            val key=binding.inputPasswordRegister.text.toString()
-            if (usu.isNotEmpty()&& key.isNotEmpty()){
-                if (key.length>=6){
-                    userRegisterFB(usu,key)
-                    val bundle=intent.extras
-                    val id:String?=bundle?.getString("dataid")
-                    println("++++++++++++++++++++++++++++++++++++++++++++++")
-                    println(bundle?.getString("dataid"))
-                    //saveDataFBStorage(id?:"")
-
-
-
-                    Toast.makeText(this,"datos guardados",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "clave debe tener mas de 6 caracteres", Toast.LENGTH_LONG)
+                        .show()
                 }
-                else{
-                    Toast.makeText(this,"clave debe tener mas de 6 caracteres",Toast.LENGTH_LONG).show()
-                }
+                //val bundle = intent.extras
+                //val correo: String? = bundle?.getString("correo")
+                //binding.inputEmailRegister.setText(email)
+
             }
-            else{
-                Toast.makeText(this,"no deje valores vacios",Toast.LENGTH_LONG).show()
-            }
-
-
-
-
-
         }
-
-
     }
 
-    val openCamera =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val data = result.data!!
-                val bitmap= BitmapFactory.decodeFile(file.toString())
-                binding.inputImageRegister.setImageBitmap(bitmap)
-            }
-        }
-
-
-    private lateinit var file: File
 
     private fun photoFile() {
         val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
         file = File.createTempFile("IMG_${System.currentTimeMillis()}_", ".jpg", dir)
     }
 
-    /*private fun createContent() :ContentValues{
-        val namefile = file.name
-        val typeFile = "image/jpg"
-        return ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, namefile)
-            put(MediaStore.Files.FileColumns.MIME_TYPE, typeFile)
-            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-            put(MediaStore.MediaColumns.IS_PENDING,1)
-        }
-    }
-
-
-    private fun saveContent(content:ContentValues):Uri{
-        var outputStream:OutputStream?
-        var uri:Uri?
-        application.contentResolver.also { resolver ->
-            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, content)
-            outputStream = resolver.openOutputStream(uri!!)
-        }
-        outputStream.use {
-            output->
-            mappingImage().compress(Bitmap.CompressFormat.JPEG, 100,output)
-        }
-        return uri!!
-    }
-
-    private fun mappingImage():Bitmap{
-        return BitmapFactory.decodeFile(file.toString())
-    }
-
-
-    private fun saveImage() {
-        val save=createContent()
-        val uri=saveContent(content)
-    }
-
-
-    private fun createPhotoFile() {
-    }*/
-
-fun saveuserDB(){
-    val user = binding.inputEmailRegister.text.toString()
-    val password=binding.inputPasswordRegister.text.toString()
-    val address=binding.inputAdressRegister.text.toString()
-    val name=binding.inputNamesRegister.text.toString()
-    val phone=binding.inputPhoneRegister.text.toString()
-    val rol=binding.inputRollRegister.text.toString()
-    val usu=UserModel(user,password,address,name,phone,rol)
-    database.userDao.insert(usu)
-
-
-    }
-
-    private fun userRegisterFB(email:String,password:String){
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this){
-            task->
-                if (task.isSuccessful){
-                    val id=firebaseAuth.uid
-                    val intent=Intent(this,Register::class.java)
-                    intent.putExtra("dataid",id)
-                    intent.putExtra("correo",email)
-                    startActivity(intent)
-                    Toast.makeText(this,"datos registrados",Toast.LENGTH_LONG).show()
-                }
-            else{
-                    Toast.makeText(this,"los datos no pueden estar vacios",Toast.LENGTH_LONG).show()
-                }
-
-        }
-    }
-
-
-    private fun capcharegisterFB(){
-
-        val bundle=intent.extras
-        val email:String?=bundle?.getString("correo")
-        binding.inputEmailRegister.setText(email)
-    }
-
-    private fun saveDataFBStorage(id:String){
+    private fun saveDataFBStorage(id: String) {
         db.collection("appUser").document(id).set(
-            hashMapOf("nombre" to binding.inputNamesRegister.text.toString(),"direccion" to binding.inputAdressRegister.text.toString(),
-            "celular" to binding.inputPhoneRegister.text.toString(),"rol" to binding.inputRollRegister.text.toString())
+            hashMapOf(
+                "nombre" to binding.inputNamesRegister.text.toString(),
+                "direccion" to binding.inputAdressRegister.text.toString(),
+                "celular" to binding.inputPhoneRegister.text.toString(),
+                "rol" to binding.inputRollRegister.text.toString()//,
+                //"picture" to binding.inputImageRegister.setImageBitmap(bitmap)
+            )
         )
-
     }
+
+
+    private fun userRegisterFB(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    id = firebaseAuth.uid
+                    id?.let { saveDataFBStorage(it) }
+                    println("/n" + "/n" + "DATOS ANTES DE SOBREESCRIBIR A CERO" + id + "/n" + "/n")
+                    id = ""
+                    println("/n" + "/n" + "DATOS despues DE SOBREESCRIBIR ID" + id + "/n" + "/n")
+
+                    Toast.makeText(this, "Datos registrados ", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, "Los datos no pueden estar vacios", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+    }
+
+    val openCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data!!
+                bitmap = BitmapFactory.decodeFile(file.toString())
+                binding.inputImageRegister.setImageBitmap(bitmap)
+            }
+        }
 
 }
